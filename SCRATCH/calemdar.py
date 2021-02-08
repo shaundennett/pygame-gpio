7a3997aac1a8e40b75646031a15c206c15131bba
shaundennett <shaun_dennett@hotmail.com>
2021-02-08 13:05:40 +0000

    Update calemdar.py

--- SCRATCH/calemdar.py
+++ SCRATCH/calemdar.py
@@ -17,7 +17,7 @@ for cal in all:
         #work out the timeslots
         for slot in range(start,end + 1):
             ts_1.append(slot)
-
+    print("hello")
     all_ts.append(ts_1)
     ts_1 = []
 
@@ -25,7 +25,7 @@ slots = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23}
 
 set_1 = set(all_ts[0] + all_ts[1] + all_ts[2])
 
-#all_used_slots = set(all_ts[0]).union(set(all_ts[1]))
+#all_used_slots = set(all_ts[0]).union(set(all_ts[1])) some more text 
 print("--------------------------")
 print(set_1)
 print(slots.symmetric_difference(set_1))
